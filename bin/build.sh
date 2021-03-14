141da498f1200bcb20e0933f4a23e267378e95ba
George Dilley <george@xigen.co.uk>
2021-03-14 11:53:48 +0000

    Add new images and updated script

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+echo "--=[ Building Node 10 ]=--"
 docker build --pull -t xigen/node:10 -f Dockerfile-10 .
-docker build --pull -t xigen/node:latest -t xigen/node:11 -f Dockerfile-11 .
+echo "--=[ Building Node 11 ]=--"
+docker build --pull -t xigen/node:11 -f Dockerfile-11 .
+echo "--=[ Building Node 12 ]=--"
+docker build --pull -t xigen/node:12 -f Dockerfile-12 .
+echo "--=[ Building Node 13 ]=--"
+docker build --pull -t xigen/node:13 -f Dockerfile-13 .
+echo "--=[ Building Node 14 ]=--"
+docker build --pull -t xigen/node:14 -f Dockerfile-14 .
+echo "--=[ Building Node 15 ]=--"
+docker build --pull -t xigen/node:15 -f Dockerfile-15 .
+echo "--=[ Building Node Latest ]=--"
+docker build --pull -t xigen/node:latest -f Dockerfile-15 .
--- bin/push.sh
+++ bin/push.sh
@@ -2,4 +2,8 @@
 
 docker push xigen/node:10
 docker push xigen/node:11
+docker push xigen/node:12
+docker push xigen/node:13
+docker push xigen/node:14
+docker push xigen/node:15
 docker push xigen/node:latest
